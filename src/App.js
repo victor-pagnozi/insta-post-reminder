e5aa159861939e51602757b97491191c5c22ccab
Victor Pagnozi <victor.pagnozi@gmail.com>
2022-10-22 14:59:33 -0300

    Done function to counter times minutes and seconds

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import { Notifier } from "./Notifier.js";
+import { Timer } from './Timer.js'
 
 const App = {
   async start() {
     try {
+      const time = 25 * 60;
+      Timer.init(time);
       await Notifier.init();
-      Notifier.notify({
-        title: 'Hora do Post',
-        body: 'Crie algum conteúdo para ajudar a comunidade'
-      });
+      // Notifier.notify({
+      //   title: 'Hora do Post',
+      //   body: 'Crie algum conteúdo para ajudar a comunidade'
+      // });
     } catch (error) {
       console.log(error.message);
     }
